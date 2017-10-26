9267622d91e378309dc595fa3fd1c9679b6689d2
dosumis <dosumis@gmail.com>
2017-10-26 12:00:44 +0100

    modified local_test function to work from directories

--- src/schema_test_suite.py
+++ src/schema_test_suite.py
@@ -3,6 +3,7 @@ from jsonschema import Draft4Validator, RefResolver, SchemaError
 import warnings
 import subprocess
 import os
+import glob
 
 
 def get_json_from_file(filename):
@@ -47,23 +48,28 @@ def recurse_through_errors(es, level = 0):
     and schema path until context is empty"""
     # Assuming blank context is a sufficient escape clause here.
     for e in es:
-        warnings.warn("***"*level + "subschema level " + str(level) + "\t".join([e.message, 
-                                     "Path to error:" +  str(e.absolute_schema_path)]) + "\n")
+        warnings.warn(
+            "***"*level + "subschema level " + str(level) + "\t".join([e.message, 
+            "Path to error:" +  str(e.absolute_schema_path)]) + "\n")
         if e.context:
             level += 1
             recurse_through_errors(e.context, level = level)
             
 
-def test_local(path_to_schema_dir, schema_file, instance_file):
-    """Tests a single instance against a single schema.
+def test_local(path_to_schema_dir, schema_file, test_dir):
+    """Tests a instances in a test_folder against a single schema.
     Assumes all schema files in single dir.
-    path_to_schema_dir may be relative.
-    schema_file and instance_file may be local to schema dir or full path."""
+    Assumes all *.json files in the test_dir should validate against the schema.
+       * path_to_schema_dir:  Absolute or relative path to schema dir
+       * schema_file: schema file name
+       * test_dir: path to test directory (absolute or local to schema dir)
+    """
     os.chdir(path_to_schema_dir)
     pwd = subprocess.check_output('pwd').decode("utf-8").rstrip()
     base_uri = "file://" + pwd + "/"
     sv = get_validator(schema_file, base_uri)
-    i = get_json_from_file(instance_file)
-    print("Testing: %s" % instance_file)
-    validate(sv, i)
-    
+    test_files = glob.glob(pathname=test_dir + '/*.json')
+    for instance_file in test_files:
+        i = get_json_from_file(instance_file)
+        print("Testing: %s" % instance_file)
+        validate(sv, i)
