cf6cef7d4244d4888de27b3ed7361d449992fb8a
Mallory Freeberg <mallory.freeberg@gmail.com>
2017-11-03 14:16:02 +0000

    Switched to sample_test.py in travis

--- src/sample_test.py
+++ src/sample_test.py
@@ -5,18 +5,28 @@ import subprocess
 # Simple first test - are json files in json_schema folder
 # valid json schema docs?
 
+## Flag for tracking the exit status of validate() calls
+status_flag = True
+
 os.chdir('../json_schema')
 pwd = subprocess.check_output('pwd').decode("utf-8").rstrip()
 base_uri = "file://" + pwd + "/"
 print(base_uri)
+
 sv = get_validator('sample.json', base_uri)
 dt1 = get_json_from_file('../schema_test_files/10x_pbmc8k_donor_0.json')
 # Specific schema tests to follow .
-validate(sv, dt1)
+if not validate(sv, dt1): ## will return False if fails (show return value)
+    status_flag = False
 
 sfo1 = get_json_from_file('../schema_test_files/10x_pbmc8k_sample_0.json')
-validate(sv, sfo1)
+if not validate(sv, sfo1):
+    status_flag = False
 
 sample_bundle_validator = get_validator('sample_bundle.json', base_uri)
 sample_bundle_file = get_json_from_file('../schema_test_files/10x_pbmc8k_sample_bundle.json')
-validate(sample_bundle_validator, sample_bundle_file)
+if not validate(sample_bundle_validator, sample_bundle_file):
+    status_flag = False
+
+if not status_flag:
+    print('validate() failed.')
