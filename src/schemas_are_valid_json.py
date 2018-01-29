679a0a86ed61b9d5c0204ad3c2e896b04362d770
Mallory Freeberg <mallory.freeberg@gmail.com>
2018-01-29 10:23:52 +0000

    Updated schema validator to recursively search directory for .json files.

--- src/schemas_are_valid_json.py
+++ src/schemas_are_valid_json.py
@@ -1,16 +1,19 @@
 from schema_test_suite import get_validator
-import glob
+import os
 
 """
 schemas_are_valid_json: 
-This code tests whether JSON schemas that are in the json_schema/* folders
-are valid JSON schema.
+This code tests whether JSON schemas that are in the json_schema/*
+directory are valid JSON schema.
 """
 
-schemas = glob.glob('../json_schema/*/' + "*.json")
-schemas.extend(glob.glob('../json_schema/*/*/' + "*.json"))
-schemas.extend(glob.glob('../json_schema/*/*/*/' + "*.json"))
-# print(len(schemas))
+schema_path = '../json_schema'
+
+schemas = [os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk(schema_path)
+    for f in files if f.endswith('.json')]
+
+print("Checking %d schemas" % len(schemas))
 
 for s in schemas:
     print("Checking whether %s is a valid json schema" % s)
