451fee5a960fc43598ce13e7062e4dd0afc74faa
Dani Welter <dwelter@ebi.ac.uk>
2018-03-08 15:27:19 +0000

    Updated json to spreadsheet script to use local file and concatenate concept-module field name for integrated names if user friendly available. Updated library_preparation_process.json to work with this

--- src/json_to_spreadsheet_template.py
+++ src/json_to_spreadsheet_template.py
@@ -3,6 +3,8 @@ import logging
 from openpyxl import Workbook
 import requests, pprint
 from openpyxl.styles import Font
+import os
+from schema_test_suite import get_json_from_file
 
 
 # hard coded tab ordering
@@ -15,12 +17,12 @@ class SpreadsheetCreator:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
-    def generateSpreadsheet(self, baseUri, schemas, dependencies, output):
+    def generateSpreadsheet(self, baseUri, schemas, dependencies, output, local):
         values = {}
         try:
             # for each schema, gather the values for the relevant tab(s)
             for schema in schemas:
-                v = self._gatherValues(baseUri+schema, dependencies)
+                v = self._gatherValues(baseUri, schema, dependencies, local)
                 values.update(v)
             # Build the spreadsheet from the retrieved values
             self._buildSpreadsheet(values, output)
@@ -28,106 +30,135 @@ class SpreadsheetCreator:
             self.logger.error("Error:" + str(e))
             raise e
 
-    def _gatherValues(self, schema, dependencies):
-        # get the schema of HTTP
-        req = requests.get(schema)
+    def _gatherValues(self, basepath, schema, dependencies, local):
 
-        # if the schema is successfully retrieved, process it, else return an error message
-        if (req.status_code == requests.codes.ok):
-            jsonRaw = req.json()
-
-            entities = {}
-            entity_title = jsonRaw["title"]
-
-            properties = jsonRaw["properties"]
-
-            values = []
-
-            for prop in properties:
-                # if a property has an array of references (potential 1-to-many relationship), gather the properties for the references and format them to become
-                # their own spreadsheet tab
-                if ("items" in properties[prop] and "$ref" in properties[prop]["items"] and "ontology" not in properties[prop]["items"]["$ref"]):
-                    module = properties[prop]["items"]["$ref"]
-                    if module in dependencies:
-                        module_values = self._gatherValues(module, None)
-                        # add primary entity ID to cross reference with main entity
-                        for primary in values:
-                            if "ID" in primary["header"] or "shortname" in primary["header"]:
-                                for key in module_values.keys():
-                                    t = primary["header"]
-                                    if "ID" in t:
-                                        t = t.replace(" ID", "").lower()
-                                        d = "ID for " + t + " this " + key + " relates to"
-                                    else:
-                                        t = t.replace(" shortname", "").lower()
-                                        d = "Shortname for " + t + " this " + key + " relates to"
-                                    module_values[key].append({"header": primary["header"],
-                                                               "description": d,
-                                                               "example": None})
-                                break
-
-                        # special name cases for publication tabs
-                        if entity_title == "project" and "publication" in module_values.keys():
-                            module_values["project.publications"] = module_values.pop("publication")
-                        if entity_title == "cell_line" and "publication" in module_values.keys():
-                            module_values["cell_line.publications"] = module_values.pop("publication")
-                        entities.update(module_values)
-                # if a property does not include a user_friendly tag but includes a reference, fetch the contents of that reference and add them
-                # directly to the properties for this sheet
-                elif("$ref" in properties[prop] and "ontology" not in properties[prop]["$ref"]):
-                    module = properties[prop]["$ref"]
-                    if "_core" in module or module in dependencies:
-                        module_values = self._gatherValues(module, None)
-                        for key in module_values.keys():
-                            # special case for naming UMI barcodes
-                            if prop == "umi_barcode":
-                                for entry in module_values[key]:
-                                    entry["header"] = "UMI " + entry["header"]
-                            # special case for naming cell barcodes
-                            if prop == "cell_barcode":
-                                for entry in module_values[key]:
-                                    entry["header"] = "Cell " + entry["header"]
-
-                            values.extend(module_values[key])
-
-                # if a property has a user_friendly tag, include it as a direct field. This includes ontology module references as these should not be
-                # exposed to users
-                elif ("user_friendly" in properties[prop]):
-                    description = None
-                    example = None
-                    if "description" in properties[prop]:
-                        description = properties[prop]["description"]
-                    if "example" in properties[prop]:
-                        example = properties[prop]["example"]
-
-                    values.append({"header": properties[prop]["user_friendly"], "description": description,
-                                   "example": example})
-
-            if "type/biomaterial" in schema:
-                values.append(
-                    {"header": "Process IDs", "description": "IDs of processes for which this biomaterial is an input",
-                                   "example": None})
-            if "type/process" in schema:
-                values.append(
-                    {"header": "Protocol IDs", "description": "IDs of protocols which this process implements",
-                     "example": None})
-            # if "module/process/purchased_reagents" in schema:
-            #     values.append(
-            #         {"header": "Process ID", "description": "ID of the process in which this reagent was used",
-            #          "example": None})
-            if "type/file" in schema:
-                values.append(
-                    {"header": "Biomaterial ID", "description": "ID of the biomaterial to which this file relates",
-                     "example": None})
-                values.append(
-                    {"header": "Sequencing process ID", "description": "ID of the sequencing process to which this file relates",
-                     "example": None})
-
-            entities[entity_title] = values
-            return entities
+        if local:
+            jsonRaw = get_json_from_file(schema)
 
         else:
-            self.logger.error(schema + " does not exist")
+            # get the schema of HTTP
+            req = requests.get(basepath+schema)
+
+            # if the schema is successfully retrieved, process it, else return an error message
+            if (req.status_code == requests.codes.ok):
+                jsonRaw = req.json()
+            else:
+                self.logger.error(schema + " does not exist")
+
+        entities = {}
+        entity_title = jsonRaw["title"]
+
+        properties = jsonRaw["properties"]
+
+        values = []
+
+        for prop in properties:
+            # if a property has an array of references (potential 1-to-many relationship), gather the properties for the references and format them to become
+            # their own spreadsheet tab
+            if ("items" in properties[prop] and "$ref" in properties[prop]["items"] and "ontology" not in properties[prop]["items"]["$ref"]):
+                module = properties[prop]["items"]["$ref"]
+                if local:
+                    el = module.split("/")
+                    del el[0:3]
+                    del el[-2]
+
+                    module = basepath
+
+                    for e in el:
+                        module = module + "/" + e
+                    module = module + ".json"
+                if module in dependencies:
+                    module_values = self._gatherValues(basepath, module, None, local)
+                    # add primary entity ID to cross reference with main entity
+                    for primary in values:
+                        if "ID" in primary["header"] or "shortname" in primary["header"]:
+                            for key in module_values.keys():
+                                t = primary["header"]
+                                if "ID" in t:
+                                    t = t.replace(" ID", "").lower()
+                                    d = "ID for " + t + " this " + key + " relates to"
+                                else:
+                                    t = t.replace(" shortname", "").lower()
+                                    d = "Shortname for " + t + " this " + key + " relates to"
+                                module_values[key].append({"header": primary["header"],
+                                                           "description": d,
+                                                           "example": None})
+                            break
+
+                    # special name cases for publication tabs
+                    if entity_title == "project" and "publication" in module_values.keys():
+                        module_values["project.publications"] = module_values.pop("publication")
+                    if entity_title == "cell_line" and "publication" in module_values.keys():
+                        module_values["cell_line.publications"] = module_values.pop("publication")
+                    entities.update(module_values)
+            # if a property does not include a user_friendly tag but includes a reference, fetch the contents of that reference and add them
+            # directly to the properties for this sheet
+            elif("$ref" in properties[prop] and "ontology" not in properties[prop]["$ref"]):
+                module = properties[prop]["$ref"]
+                if local:
+                    el = module.split("/")
+                    del el[0:3]
+                    del el[-2]
+
+                    module = basepath
+
+                    for e in el:
+                        module = module + "/" + e
+                    module = module + ".json"
+
+                    if "_core" in module or module in dependencies:
+                        module_values = self._gatherValues(basepath, module, None, local)
+
+                    prefix = ""
+                    if module in dependencies:
+
+                        if "user_friendly" in properties[prop]:
+                            prefix = properties[prop]["user_friendly"] + " - "
+                        else:
+                            print(prop + " in " + entity_title + " has no user friendly name")
+
+                    for key in module_values.keys():
+                        for entry in module_values[key]:
+                            entry["header"] = prefix + entry["header"]
+                        values.extend(module_values[key])
+
+
+
+
+
+            # if a property has a user_friendly tag, include it as a direct field. This includes ontology module references as these should not be
+            # exposed to users
+            elif ("user_friendly" in properties[prop]):
+                description = None
+                example = None
+                if "description" in properties[prop]:
+                    description = properties[prop]["description"]
+                if "example" in properties[prop]:
+                    example = properties[prop]["example"]
+
+                values.append({"header": properties[prop]["user_friendly"], "description": description,
+                               "example": example})
+
+        if "type/biomaterial" in schema:
+            values.append(
+                {"header": "Process IDs", "description": "IDs of processes for which this biomaterial is an input",
+                               "example": None})
+        if "type/process" in schema:
+            values.append(
+                {"header": "Protocol IDs", "description": "IDs of protocols which this process implements",
+                 "example": None})
+        if "type/file" in schema:
+            values.append(
+                {"header": "Biomaterial ID", "description": "ID of the biomaterial to which this file relates",
+                 "example": None})
+            values.append(
+                {"header": "Sequencing process ID", "description": "ID of the sequencing process to which this file relates",
+                 "example": None})
+
+        entities[entity_title] = values
+        return entities
+
+
 
 
     def _buildSpreadsheet(self, values, outputLocation):
@@ -182,6 +213,8 @@ if __name__ == '__main__':
                       help="Schema types to include in the spreadsheet")
     parser.add_option("-i", "--include", dest="include",
                       help="Schema modules to include in the spreadsheet")
+    parser.add_option("-l", "--local", action="store_true", dest="local_files",
+                      help="Run using local schemas")
 
 
     (options, args) = parser.parse_args()
@@ -190,14 +223,42 @@ if __name__ == '__main__':
     #     print ("You must supply a base schema URI for the metadata")
     #     exit(2)
 
-    schema_types = options.schema_types.split(",")
-    dependencies = options.include.split(",")
+    base_schema_path = options.schema_uri
 
-    for index, dependency in enumerate(dependencies):
-        dependencies[index] = options.schema_uri+dependency
+    if options.local_files:
+
+        core_schema_path = base_schema_path + '/core'
+        type_schema_path = base_schema_path + '/type'
+        module_schema_path = base_schema_path + '/module'
+
+        core_schemas = [os.path.join(dirpath, f)
+                        for dirpath, dirnames, files in os.walk(core_schema_path)
+                        for f in files if f.endswith('.json')]
+
+        schema_types = [os.path.join(dirpath, f)
+                        for dirpath, dirnames, files in os.walk(type_schema_path)
+                        for f in files if f.endswith('.json')]
+        dependencies = [os.path.join(dirpath, f)
+                          for dirpath, dirnames, files in os.walk(module_schema_path)
+                          for f in files if f.endswith('.json')]
+
+        ontologies = []
+
+        for d in dependencies:
+            if d.endswith('_ontology.json'):
+                ontologies.append(d)
+                dependencies.remove(d)
+
+
+    else:
+        schema_types = options.schema_types.split(",")
+        dependencies = options.include.split(",")
+
+        for index, dependency in enumerate(dependencies):
+            dependencies[index] = options.schema_uri+dependency
 
     generator = SpreadsheetCreator()
-    generator.generateSpreadsheet(options.schema_uri, schema_types, dependencies, options.output)
+    generator.generateSpreadsheet(base_schema_path, schema_types, dependencies, options.output, options.local_files)
 
 
 
@@ -211,4 +272,9 @@ if __name__ == '__main__':
 # -s "https://raw.githubusercontent.com/HumanCellAtlas/metadata-schema/v5_prototype/json_schema/"
 # -t "type/project/project.json,type/biomaterial/donor_organism.json,type/biomaterial/specimen_from_organism.json,type/biomaterial/cell_suspension.json,type/biomaterial/cell_line.json,type/biomaterial/organoid.json,type/process/biomaterial_collection/collection_process.json,type/process/biomaterial_collection/dissociation_process.json,type/process/biomaterial_collection/enrichment_process.json,type/process/sequencing/library_preparation_process.json,type/process/sequencing/sequencing_process.json,type/protocol/protocol.json,type/file/sequence_file.json"
 # -i "module/project/contact.json,module/project/publication.json,module/biomaterial/cell_morphology.json,module/biomaterial/death.json,module/biomaterial/homo_sapiens_specific.json,module/biomaterial/medical_history.json,module/biomaterial/mus_musculus_specific.json,module/biomaterial/state_of_specimen.json,module/biomaterial/familial_relationship.json,module/process/sequencing/barcode.json,module/process/sequencing/smartseq2.json"
+# -o "/Users/dwelter/Development/HCA/metadata-schema/src/spreadsheet_test.xlsx"
+
+# Run using local files only
+# -s "../json"
+# -l
 # -o "/Users/dwelter/Development/HCA/metadata-schema/src/spreadsheet_test.xlsx"
