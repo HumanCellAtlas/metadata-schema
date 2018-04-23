4d3b39396d4c152603b7ec671ba642cbb20faf35
Dani Welter <dwelter@ebi.ac.uk>
2018-04-23 17:01:01 +0100

    jsonBrowser now has additional required_fields doc. describedBy and schema_version moved out of tables to the top of each doc

--- src/human_readable_json.py
+++ src/human_readable_json.py
@@ -4,16 +4,37 @@ import os
 from schema_test_suite import get_json_from_file
 
 
-
+boiler_plate = ["describedBy", "schema_version"]
 
 class MarkdownGenerator:
 
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
+        self.required_file = open("../docs/jsonBrowser/required_fields.md", "w")
+        self.required_file.write("# Required fields\n")
+        self.required_file.write("_This document contains a list of all required fields in the HCA metadata schema. For a full"
+                                 "description of each schema, please refer to the relevant entity specification document._\n")
+
+
+    def _addBoilerPlate(self, file):
+        file.write("## Common fields\n")
+        file.write("_Fields common to all schemas in this document_\n")
+        file.write("\n")
+        file.write("Property name | Description | Type | Required? | Example \n"
+                   "--- | --- | --- | --- | ---\n "
+                   "describedBy | The URL reference to the schema. | string | no |  |  |  | \n"
+                   "schema_version | The version number of the schema in major.minor.patch format. | string | no | 4.6.1\n"
+                   "\n"
+)
+
     def generateMarkdown(self, schemas, entity_type):
         file = open("../docs/jsonBrowser/" + entity_type + ".md", "w")
         file.write("# " + entity_type.capitalize() + "\n")
+
+        self.required_file.write("## " + entity_type.capitalize() + "\n")
+        
+        self._addBoilerPlate(file)
 
         for path in schemas:
 
@@ -21,8 +42,11 @@ class MarkdownGenerator:
 
             if (entity_type == "module" or entity_type == "core"):
                 file.write("## " + schema["title"] + "<a name='" + schema["title"] + "'></a>\n")
+                self.required_file.write("### " + schema["title"] + "<a name='" + schema["title"] + "'></a>\n")
+
             else:
                 file.write("## " + schema["title"] + "\n")
+                self.required_file.write("### " + schema["title"] + "\n")
             file.write("_" + schema["description"] + "_\n")
             file.write("\n")
             file.write("Location: " +  path.replace("../json_schema/", "") + "\n")
@@ -37,53 +61,75 @@ class MarkdownGenerator:
             if "required" in schema:
                 required = schema["required"]
 
+                self.required_file.write(
+                    "Property name | Description | Type | Object reference? | User friendly name | Allowed values | Example \n")
+                self.required_file.write("--- | --- | --- | --- | --- | --- | --- \n")
+
+            else:
+                self.required_file.write("_There are no required properties in schema " + schema["title"] + "_\n")
+
             for property in schema["properties"]:
 
-                if "$ref" in schema["properties"][property]:
-                    ref = schema["properties"][property]["$ref"]
-                    if "definitions" not in ref:
-                        if "core" in ref:
-                            dir = "core"
-                        elif "module" in ref:
-                            dir = "module"
+                if property not in boiler_plate:
+
+                    if "$ref" in schema["properties"][property]:
+                        ref = schema["properties"][property]["$ref"]
+                        if "definitions" not in ref:
+                            if "core" in ref:
+                                dir = "core"
+                            elif "module" in ref:
+                                dir = "module"
+                            else:
+                                dir = ""
+                            mod = ref.split("/")[-1]
+                            mod = mod.replace(".json", "")
+                            link = "[See " + dir + "  " + mod + "](" + dir + ".md/#" + mod + ")"
                         else:
-                            dir = ""
-                        mod = ref.split("/")[-1]
-                        mod = mod.replace(".json", "")
-                        link = "[See " + dir + "  " + mod + "](" + dir + ".md/#" + mod + ")"
+                            link = ""
+
+
+                    elif "items" in schema["properties"][property] and "$ref" in schema["properties"][property]["items"]:
+                        ref = schema["properties"][property]["items"]["$ref"]
+                        if "definitions" not in ref:
+                            if "core" in ref:
+                                dir = "core"
+                            elif "module" in ref:
+                                dir = "module"
+                            else:
+                                dir = ""
+                            mod = ref.split("/")[-1]
+                            mod = mod.replace(".json", "")
+                            link = "[See " + dir + "  " + mod + "](" + dir + ".md/#" + mod + ")"
+                        else:
+                            link = ""
                     else:
                         link = ""
 
+                    # if link is not "":
+                    #     print(schema["title"] + "\t "+ property + "\t"+ link)
 
-                elif "items" in schema["properties"][property] and "$ref" in schema["properties"][property]["items"]:
-                    ref = schema["properties"][property]["items"]["$ref"]
-                    if "definitions" not in ref:
-                        if "core" in ref:
-                            dir = "core"
-                        elif "module" in ref:
-                            dir = "module"
-                        else:
-                            dir = ""
-                        mod = ref.split("/")[-1]
-                        mod = mod.replace(".json", "")
-                        link = "[See " + dir + "  " + mod + "](" + dir + ".md/#" + mod + ")"
-                    else:
-                        link = ""
-                else:
-                    link = ""
+                    file.write(property + " | "+
+                               (schema["properties"][property]["description"] if "description" in schema["properties"][property] else "") + " | " +
+                               (schema["properties"][property]["type"] if "type" in schema["properties"][property] else "")  + " | " +
+                               ("yes" if property in required else "no")  + " | " +
+                               link + " | " +
+                               (schema["properties"][property]["user_friendly"] if "user_friendly" in schema["properties"][property] else "") + " | " +
+                               (", ".join(schema["properties"][property]["enum"]) if "enum" in schema["properties"][property] else "") + " | " +
+                               (str(schema["properties"][property]["example"]) if "example" in schema["properties"][property] else "") + "\n")
 
-                # if link is not "":
-                #     print(schema["title"] + "\t "+ property + "\t"+ link)
-
-                file.write(property + " | "+
-                           (schema["properties"][property]["description"] if "description" in schema["properties"][property] else "") + " | " +
-                           (schema["properties"][property]["type"] if "type" in schema["properties"][property] else "")  + " | " +
-                           ("yes" if property in required else "no")  + " | " +
-                           link + " | " +
-                           (schema["properties"][property]["user_friendly"] if "user_friendly" in schema["properties"][property] else "") + " | " +
-                           (", ".join(schema["properties"][property]["enum"]) if "enum" in schema["properties"][property] else "") + " | " +
-                           (str(schema["properties"][property]["example"]) if "example" in schema["properties"][property] else "") + "\n")
-
+                    if property in required:
+                        self.required_file.write(property + " | " +
+                            (schema["properties"][property]["description"] if "description" in schema["properties"][
+                                property] else "") + " | " +
+                            (schema["properties"][property]["type"] if "type" in schema["properties"][
+                                property] else "") + " | " +
+                            link + " | " +
+                            (schema["properties"][property]["user_friendly"] if "user_friendly" in schema["properties"][
+                                property] else "") + " | " +
+                            (", ".join(schema["properties"][property]["enum"]) if "enum" in schema["properties"][
+                                property] else "") + " | " +
+                            (str(schema["properties"][property]["example"]) if "example" in schema["properties"][
+                                property] else "") + "\n")
 
 
 
