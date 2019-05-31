296ff49aaffc854a6f95db1511c565496836ea97
Mallory Freeberg <mallory.freeberg@gmail.com>
2019-05-31 23:02:39 +0100

    TESTING FAKE SCHEMA UPDATE.

--- src/schema_linter.py
+++ src/schema_linter.py
@@ -3,7 +3,6 @@ import logging
 import os
 import re
 import json
-import sys
 
 
 allowed_root_level_keywords = ['$schema', 'description', 'additionalProperties', 'required', 'title', 'name', 'type', 'properties', 'definitions']
